2f30924fd55228907c301744fd032cb011e89915
Manikant Mishra <manimishra2006@gmail.com>
2026-04-08 10:42:37 +0530

    Refactor code structure for improved readability and maintainability

---
+++ REPO 1(Crud Operations)/server.js
@@ -0,0 +1,51 @@
+const express=require('express');
+const app=express();
+
+app.use(express.json());
+
+let notes=[];
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+});
+
+app.post('/notes',(req,res)=>{
+    console.log(req.body);
+    notes.push(req.body);
+    res.json({message:'Note added successfully'});
+});
+
+app.get('/notes',(req,res)=>{
+    res.json(notes);
+});
+
+
+
+app.delete('/notes/:index',(req,res)=>{
+    const index=req.params.index;
+    delete notes[index];
+    res.json({message:'Note deleted successfully'});
+});
+
+
+
+
+
+app.patch('/notes/:index',(req,res)=>{
+    const index=req.params.index;
+    const {title}=req.body;
+    notes[index].title=title;
+    res.json({message:'Note updated successfully'});
+});
+
+app.put('/notes/:index',(req,res)=>{
+    const index=req.params.index;
+    const {title,content}=req.body;
+    notes[index]={title,content};
+    res.json({message:'Note replaced successfully'});
+});
+
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
